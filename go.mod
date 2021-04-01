72ab1d15cde1bd5d622965e8357085ccf9d1a5f3
Haze <50791031+Haze-Lan@users.noreply.github.com>
2021-04-01 16:32:14 +0800

    初始化库

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Haze-Lan/haze-go
+
+go 1.16
+
+require google.golang.org/grpc v1.36.1 // indirect
+
+replace google.golang.org/grpc => github.com/grpc/grpc-go latest
---
+++ haze.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	 pb "github.com/Haze-Lan/haze-go/api/hellworld"
+	"log"
+	"net"
+
+)
+
+
+
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+
